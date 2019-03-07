90e1562269097d45f3f69ced3f24010ae16b9e02
Dennis Wenrich <dennisfinley@gmail.com>
2019-03-06 23:23:06 -0500

    Added documentation for js file

--- 4 - DOM/Dicee Challenge - Starting Files/index.js
+++ 4 - DOM/Dicee Challenge - Starting Files/index.js
@@ -1,3 +1,5 @@
+//random numbers for the first dice
 var randomNumber1 = Math.floor((Math.random() * 6) +1);
 
+//selects the first dice picture and sets random numbers to replace the images on reload
 document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
