873db467fe8e2d90b77ca8cc16ea6a7c2ee82ec6
Dennis Wenrich <dennisfinley@gmail.com>
2019-03-06 23:26:09 -0500

    Added random numbers for second dice / set random images for the right picture

--- 4 - DOM/Dicee Challenge - Starting Files/index.js
+++ 4 - DOM/Dicee Challenge - Starting Files/index.js
@@ -1,5 +1,11 @@
 //random numbers for the first dice
 var randomNumber1 = Math.floor((Math.random() * 6) +1);
 
+//random numbers for the second dice
+var randomNumber2 = Math.floor((Math.random() * 6) +1);
+
 //selects the first dice picture and sets random numbers to replace the images on reload
 document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+
+//selects the second dice picture and sets random numbers to replace the images on reload
+document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
