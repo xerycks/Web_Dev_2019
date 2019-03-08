a47e9a29edc5ba5938e26e764e76a4f59e5a24a3
Dennis Wenrich <dennisfinley@gmail.com>
2019-03-07 22:24:58 -0500

    Added for loop for the event listener for each button

--- 5 - Advanced DOM/Drum Kit Starting Files/index.js
+++ 5 - Advanced DOM/Drum Kit Starting Files/index.js
@@ -1,5 +1,8 @@
+var numOfButtons = document.querySelectorAll(".drum").length;
 
-document.querySelector(".set").addEventListener('click', () => {
-    alert("Hello");
-});
+for (var i = 0; i < numOfButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener('click', () => {
+        alert("Hello");
+    });
+}
 
