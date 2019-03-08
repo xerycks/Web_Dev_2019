319d4c344b67cedc6e5dae494f9eddcaefdfdf9d
Dennis Wenrich <dennisfinley@gmail.com>
2019-03-07 21:39:09 -0500

    Made an if / else statment to determine the winner of the dice

--- 4 - DOM/Dicee Challenge - Starting Files/index.js
+++ 4 - DOM/Dicee Challenge - Starting Files/index.js
@@ -9,3 +9,12 @@ document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+
 
 //selects the second dice picture and sets random numbers to replace the images on reload
 document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+
+//Checks to see which dice had the higher number to determine the winner / else if they are equal and chooses a draw
+if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw";
+} else {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
