ff90c39114eb968610faba0ecad5b76cb30c96c0
Dennis Wenrich <dennisfinley@gmail.com>
2019-03-12 11:54:19 -0400

    Added switch statement for each corresponding sound

--- 5 - Advanced DOM/Drum Kit Starting Files/index.js
+++ 5 - Advanced DOM/Drum Kit Starting Files/index.js
@@ -1,9 +1,18 @@
 var numOfButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numOfButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener('click', () => {
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+        
+            default:
+                break;
+        }
+        
     });
 }
 
